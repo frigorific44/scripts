6b2749517ce571d21b2a08c264a6f94b0af71e16
Derek Enlow <derekenlow@gmail.com>
2025-12-31 00:53:16 -0700

    Moved graphical user notification to separate script.

--- update-qbt-port.sh
+++ update-qbt-port.sh
@@ -7,12 +7,7 @@ OLD_PORT="$(curl "http://localhost:8080/api/v2/app/preferences" | jq '.listen_po
 if [[ "$OLD_PORT" != "$1" ]]; then
 	curl "http://localhost:8080/api/v2/app/setPreferences" -d "json={\"listen_port\": $1}"
 	echo "Port changed from $OLD_PORT to $1."
-	for id in $(loginctl list-sessions -j | jq -r '.[] | .session') ; do
-		if [[ $(loginctl show-session $id --property=Type) =~ (wayland|x11) ]] ; then 
-			systemd-run --machine=$(loginctl show-session $id --property=Name --value)@.host --user \
-			notify-send 'QBittorrent Port Changed' "Port changed from $OLD_PORT to $1."
-		fi
-	done
+	/usr/local/bin/notification.sh 'QBittorrent Port Changed' "Port changed from $OLD_PORT to $1."
 else
 	echo "Port unchanged from $OLD_PORT."
 fi
